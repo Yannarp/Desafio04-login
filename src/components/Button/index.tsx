9880239b76f031b8d588ed94a476a32c95da9d18
Yannarp <yannarp@hotmail.com>
2022-12-14 17:17:17 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ disabled, title, onClick }: IButtonProps) => {
+  return <ButtonContainer onClick={onClick} disabled={disabled}>
+            { title }
+         </ButtonContainer>;
+};
+
+export default Button;
